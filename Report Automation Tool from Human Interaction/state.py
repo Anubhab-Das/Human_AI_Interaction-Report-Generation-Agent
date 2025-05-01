49db9cc33eb9ce875b9fcd9709b36282cc4fa390
Anubhab Das. <anubhab.99d@gmail.com>
2025-05-01 07:22:49 +0530

    Delete Report Automation Tool from Human Interaction directory

--- Report Automation Tool from Human Interaction/state.py
+++
@@ -1,8 +0,0 @@
-from typing import Annotated, List
-from typing_extensions import TypedDict
-from langgraph.graph.message import add_messages
-from langchain_core.messages import BaseMessage
-
-class State(TypedDict):
-    messages: Annotated[List[BaseMessage], add_messages]
-    classification: str
